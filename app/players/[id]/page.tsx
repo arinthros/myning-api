7ba8d49e7f8968aa73300d6349eea46274bd753e
Caleb Robinson <caleb.robinson@life.church>
2023-08-11 11:52:00 -0500

    Fix stats key

--- app/players/[id]/page.tsx
+++ app/players/[id]/page.tsx
@@ -1,27 +1,37 @@
 import prisma from "@/lib/prisma";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const player = await prisma.player.findUnique({where: { id: parseInt(params.id, 10)}, include: { playerStats: true }})
+  const player = await prisma.player.findUnique({
+    where: { id: parseInt(params.id, 10) },
+    include: { stats: true },
+  });
 
   if (!player) {
-    return <>404 not found</>
+    return <>404 not found</>;
   }
 
   return (
     <div>
       <h1>{player.name}</h1>
-      {player.playerStats ? 
+      {player.stats ? (
         <ul>
-          {Object.entries(player.playerStats).reduce((stats: Array<React.ReactElement>, playerStat) => {
-            if (['id', 'playerId'].includes(playerStat[0])) {
-              return stats
-            } else {
-              stats.push(<li><strong>{playerStat[0]}:</strong> {playerStat[1]}</li>)
-            }
-            return stats
-          }, [])}
+          {Object.entries(player.stats).reduce(
+            (stats: Array<React.ReactElement>, playerStat) => {
+              if (["id", "player_id"].includes(playerStat[0])) {
+                return stats;
+              } else {
+                stats.push(
+                  <li>
+                    <strong>{playerStat[0]}:</strong> {playerStat[1]}
+                  </li>
+                );
+              }
+              return stats;
+            },
+            []
+          )}
         </ul>
-      : null}
+      ) : null}
     </div>
-  )
+  );
 }
